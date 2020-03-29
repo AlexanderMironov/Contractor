9a3a8539476ad6846d257c9c1ad5bad01a2e8178
AlexanderMironov <alexander_mironov@yahoo.com>
2020-03-29 20:50:45 +0200

    Add minimal launch diagnostic

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTextStream>
 //
 #include "config/configuration.h"
 #include "dbsupport/dbconnectionmanager.h"
@@ -14,12 +15,15 @@
 
 int main(int argc, char *argv[])
 {
+    QTextStream str(stdout);
     QApplication a(argc, argv);
     //init configuration
     if (Configuration::getInstance().init() == false)
     {
+        str<<"Can not initialize configuration"<<endl;
         return -1;
     };
+    str<<"agency processor initialized"<<endl;
     //
     const bool b_init_pool = DbConnectionManager::getInstance().init(2,
                                                                      Configuration::getInstance().getDBDatabaseType(),
@@ -29,40 +33,58 @@ int main(int argc, char *argv[])
                                                                      Configuration::getInstance().getDBUserPassword(),
                                                                      Configuration::getInstance().getDBPort());
     if (false == b_init_pool){
+        str<<"Can not initialize DB connection pool"<<endl;
         return -1;
     };
+    str<<"DB connection pool initialized"<<endl;
     //
     if (AgencyProcessor::getInstance().init() == false) {
+        str<<"Can not initialize agency processor"<<endl;
         return -1;
     };
+    str<<"agency processor initialized"<<endl;
     //
     if (AgentProcessor::getInstance().init() == false) {
+        str<<"Can not initialize agent processor"<<endl;
         return -1;
     };
+    str<<"agent processor initialized"<<endl;
     //
     if (CountryProcessor::getInstance().init() == false) {
+        str<<"Can not initialize country processor"<<endl;
         return -1;
     };
+    str<<"country processor initialized"<<endl;
     //
     if (OfferSkillProcesor::getInstance().init() == false) {
+        str<<"Can not initialize offer&skill processor"<<endl;
         return -1;
     };
+    str<<"offer&skill processor initialized"<<endl;
     //
     if (SkillProcessor::getInstance().init() == false) {
+        str<<"Can not initialize skill processor"<<endl;
         return -1;
     };
+    str<<"skill processor initialized"<<endl;
     //
     if (TownProcessor::getInstance().init() == false) {
+        str<<"Can not initialize town processor"<<endl;
         return -1;
     };
+    str<<"town processor initialized"<<endl;
     //
     if (StatusProcessor::getInstance().init() == false) {
+        str<<"Can not initialize status processor"<<endl;
         return -1;
     };
+    str<<"status processor initialized"<<endl;
     //
     if (OfferProcessor::getInstance().init() == false) {
+        str<<"Can not initialize offer processor"<<endl;
         return -1;
     };
+    str<<"offer processor initialized"<<endl;
     //
     MainWindow w;
     w.show();
